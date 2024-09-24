b57801eac20aee5ee8c7e70760c31cf265bddb93
Mykhailo Hozhyi <gozhenky@gmail.com>
2024-09-24 14:41:47 +0300

    add classes task-3

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,22 +1,22 @@
-import './TransactionHistory.module.css';
+import styles from './TransactionHistory.module.css';
 
 export default function TransactionHistory({ items }) {
   return (
-    <table>
-      <thead>
+    <table className={styles.table}>
+      <thead className={styles.head}>
         <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
+          <th className={styles.headItem}>Type</th>
+          <th className={styles.headItem}>Amount</th>
+          <th className={styles.headItem}>Currency</th>
         </tr>
       </thead>
 
       <tbody>
         {items.map(item => (
-          <tr key={item.id}>
-            <td>{item.type}</td>
-            <td>{item.amount}</td>
-            <td>{item.currency}</td>
+          <tr className={styles.transaction} key={item.id}>
+            <td className={styles.bodyItem}>{item.type}</td>
+            <td className={styles.bodyItem}>{item.amount}</td>
+            <td className={styles.bodyItem}>{item.currency}</td>
           </tr>
         ))}
       </tbody>
